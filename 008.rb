80291cc3ab7ef37fe476f086fb222a65aa06696e
王信凱 <jcskyting@gmail.com>
2016-11-17 20:50:35 +0800

    008 basic duck typing

---
+++ 008.rb
@@ -0,0 +1,17 @@
+class MediaPlayer
+  def play(media_player)
+    media_player.play
+  end
+end
+
+class AudioPlayer
+  def play
+    "play audio"
+  end
+end
+
+class VideoPlayer
+  def play
+    "play video"
+  end
+end
